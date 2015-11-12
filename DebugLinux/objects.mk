65783dbce765dc79ec3553f349ee56d90968aca8
ordinarygithubuser <madseahorse@yandex.com>
2015-11-12 13:20:42 +0100

    Cleaned Project because an import is impossible.

--- DebugLinux/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lscg3 -lglfw3 -lGL -lX11 -lXxf86vm -lXrandr -lpthread -lXi -lrt -ldl -lXcursor -lXinerama
-
