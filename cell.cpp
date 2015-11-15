53313489e6d137f7466ec43d959aedef802b4da5
Shadoka <tilmann.groeger@gmx.de>
2015-11-15 13:00:44 +0100

    all walls are now being rendered. still very primitive.

--- cell.cpp
+++ cell.cpp
@@ -11,7 +11,7 @@ GroupSP createCell(int col, int row, maze::Maze m) {
 	matYellow->setAmbientAndDiffuse(glm::vec4(1.0f, 1.0f, 0.0f, 1.0f))
 			 ->init();
 	auto matGreen = MaterialCore::create();
-	matGreen->setAmbientAndDiffuse(glm::vec4(0.1f, 0.8f, 0.3f, 1.f))
+	matGreen->setAmbientAndDiffuse(glm::vec4(0.1f, 0.8f, 0.3f, 1.0f))
 			->init();
 	float halfWidth = CELL_WIDTH / 2;
 	// floor
@@ -26,24 +26,22 @@ GroupSP createCell(int col, int row, maze::Maze m) {
 	floorTrans->translate(glm::vec3(col * CELL_WIDTH, 0, row * CELL_WIDTH));
 
 	//walls
-	auto wallShape = Shape::create(floorCore);
-	wallShape->addCore(matGreen);
-	TransformationSP wallTrans[2];
-	for (int i = 0; i < 2; i++) {
+	auto wallCore = geometryFactory.createCuboid(glm::vec3(CELL_WIDTH, halfWidth, 0.5f));
+	ShapeSP wallShapes[4];
+	TransformationSP wallTrans[4];
+	for (int i = 0; i < 4; i++) {
+		wallShapes[i] = Shape::create(wallCore);
+		wallShapes[i]->addCore(matGreen);
 		wallTrans[i] = Transformation::create();
-		wallTrans[i]->addChild(wallShape);
+		wallTrans[i]->addChild(wallShapes[i]);
 		cellGroup->addChild(wallTrans[i]);
 	}
-	wallTrans[0]->translate(glm::vec3(col * CELL_WIDTH - halfWidth, 0, row * CELL_WIDTH))
-				->rotate(90.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-	wallTrans[1]->translate(glm::vec3(col * CELL_WIDTH + halfWidth, 0, row * CELL_WIDTH))
-				->rotate(90.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-//	wallTrans[2]->translate(glm::vec3(col * CELL_WIDTH, 0, row * CELL_WIDTH - halfWidth))
-//				->rotate(90.0f, glm::vec3(0.0f, 0.0f, 1.0f))
-//				->rotate(90.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-//	wallTrans[3]->translate(glm::vec3(col * CELL_WIDTH + CELL_WIDTH / 2, 0, row * CELL_WIDTH))
-//				->rotate(90.0f, glm::vec3(0.0f, 0.0f, 1.0f))
-//				->rotate(90.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+	wallTrans[0]->translate(glm::vec3(col * CELL_WIDTH, halfWidth / 2, row * CELL_WIDTH - halfWidth));
+	wallTrans[1]->translate(glm::vec3(col * CELL_WIDTH, halfWidth / 2, row * CELL_WIDTH + halfWidth));
+	wallTrans[2]->translate(glm::vec3(col * CELL_WIDTH - halfWidth, halfWidth / 2, row * CELL_WIDTH))
+				->rotate(90.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+	wallTrans[3]->translate(glm::vec3(col * CELL_WIDTH + halfWidth, halfWidth / 2, row * CELL_WIDTH))
+				->rotate(90.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 
 	cellGroup->addChild(floorTrans);
 	return cellGroup;
